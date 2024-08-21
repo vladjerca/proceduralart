0e85770452d0f741803c49308b10e8e4b4fa255e
Vlad Jerca <vlad.jerca@gmail.com>
2024-08-21 19:50:51 +0200

    feat: optimize star rendering and add lens flare effect

--- renderer/internal/renderStars.mjs
+++ renderer/internal/renderStars.mjs
@@ -1,4 +1,4 @@
-import { randomInt } from '../../utils/rng.mjs';
+import { randomBool, randomInt } from '../../utils/rng.mjs';
 import { setPixel } from './canvas/setPixel.mjs';
 import { readBuffer } from './canvas/readBuffer.mjs';
 import { applyBuffer } from './canvas/applyBuffer.mjs';
@@ -28,12 +28,29 @@ export function renderStars({
         const x = randomInt(`star_x_${i}`, 0, width);
         const y = randomInt(`star_y_${i}`, 0, height);
 
-        const maxTerrainHeight = height * Math.min(
-            ...calculateTerrainHeight(x, width)
-        );
+        const size = randomInt(`star_size_${i}`, 1, 3);
+        const isLensFlared = randomBool(`star_lens_flare_${i}`, 0.3);
 
-        if (y > maxTerrainHeight) {
-            continue;
+
+        for (let dx = -size; dx <= size; dx++) {
+            for (let dy = -size; dy <= size; dy++) {
+                if (Math.hypot(dx, dy) <= size) {
+                    setPixel(data, x + dx, y + dy, colors.star.toRgb());
+                }
+            }
+        }
+
+        if (isLensFlared) {
+            for (let angle = 15; angle < 360; angle += 90) {
+                const radians = angle * (Math.PI / 180);
+                const lineLength = size * 2;
+
+                for (let dist = 0; dist < lineLength; dist++) {
+                    const dx = Math.round(dist * Math.cos(radians));
+                    const dy = Math.round(dist * Math.sin(radians));
+                    setPixel(data, x + dx, y + dy, colors.star.toRgb());
+                }
+            }
         }
 
         setPixel(data, x, y, colors.star.toRgb());
